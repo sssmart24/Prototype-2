852c7a27e73587e4ae95ccade5f1152eb8808e37
sssmart24 <ssmart24@aoiths.org>
2022-10-07 12:41:55 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x < -xRange)
+        {
+ transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * speed);
+        }
+}
