0f9e67b5488fc8150fb696275e2545922d491ba4
sssmart24 <ssmart24@aoiths.org>
2022-10-28 12:12:35 -0400

    1.4

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
+
+    private float topBound = 30;
+    private float lowerBound  = -10;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
-    private float topBound = 30;
-    private float lowerBound = -10;
-
     // Update is called once per frame
-    void Update()
-    {
-        if (transform.position.z > topBound)
+    void Update() {
+    if(transform.position.z > topBound)
         {
             Destroy(gameObject);
-        }
-        else if (transform.position.z < lowerBound)
+        } else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
